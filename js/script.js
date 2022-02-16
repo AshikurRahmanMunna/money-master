54626892edfb1527e65205cc847bcadeff33acee
AshikurRahmanMunna <ashikurrahmanmunna3@gmail.com>
2022-02-17 01:00:53 +0600

    Completed Balance Calculate

--- js/script.js
+++ js/script.js
@@ -6,5 +6,20 @@ function getAndCalculateExpenseTotal() {
     const clothsInput = document.getElementById('cloths-input');
     const clothsCost = parseFloat(clothsInput.value)
     const total = foodCost + rentCost + clothsCost;
+    const totalField = document.getElementById('expense');
+    totalField.innerText = total;
     return total;
 }
+function calculateBalance() {
+    const income = document.getElementById('income-input');
+    const incomeAmount = parseFloat(income.value);
+    const expenseAmount = getAndCalculateExpenseTotal();
+    const balance = incomeAmount - expenseAmount;
+    const balanceField = document.getElementById('balance');
+    balanceField.innerText = balance;
+}
+
+document.getElementById('calculate').addEventListener('click', function() {
+    getAndCalculateExpenseTotal();
+    calculateBalance();
+})
