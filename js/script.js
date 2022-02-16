92c77a872376425fa228d47eebe5975be4607315
AshikurRahmanMunna <ashikurrahmanmunna3@gmail.com>
2022-02-17 03:26:48 +0600

    Completed All And Fix Bugs

--- js/script.js
+++ js/script.js
@@ -1,5 +1,7 @@
-// error message
+// needed variables
 const errorMessage = document.getElementById('error-message');
+const totalExpenseField = document.getElementById('expense');
+const totalBalanceField = document.getElementById('balance');
 // Calculate and show expense total
 function getAndCalculateExpenseTotal() {
     const foodInput = document.getElementById('food-input');
@@ -8,6 +10,8 @@ function getAndCalculateExpenseTotal() {
     const rentCost = parseFloat(rentInput.value);
     const clothsInput = document.getElementById('cloths-input');
     const clothsCost = parseFloat(clothsInput.value)
+    // Handle errors
+    // The input type is number. So there are no option to type string in input. This why i have used one error handling.
     if(foodCost < 0) {
         errorMessage.innerText = 'Please Input a positive number in Food Field';
     }
@@ -29,17 +33,18 @@ function calculateBalance() {
     const income = document.getElementById('income-input');
     const incomeAmount = parseFloat(income.value);
     const expenseAmount = getAndCalculateExpenseTotal();
-    const balance = incomeAmount - expenseAmount;
     const balanceField = document.getElementById('balance');
+    const balance = incomeAmount - expenseAmount;
     balanceField.innerText = balance;
+    if(isNaN(balanceField.innerText)) {
+        balanceField.innerText = '0';
+    }
     return incomeAmount;
 }
-
+// Error Handler and combinder
 function incomeExpense() {
-    const totalExpenseField = document.getElementById('expense');
-    const totalBalanceField = document.getElementById('balance');
     if(calculateBalance() < getAndCalculateExpenseTotal()) {
-        errorMessage.innerText = "You doesn't have enought money to expense";
+        errorMessage.innerText = "You don't have enought money to expense";
         totalExpenseField.innerText = '0';
         totalBalanceField.innerText = '0';
     }
@@ -49,8 +54,37 @@ function incomeExpense() {
         calculateBalance();
     }
 }
+// Claculate the save option
+function calculateSave() {
+    const balance = document.getElementById('balance');
+    const balanceAmount = parseFloat(balance.innerText);
+    const income = calculateBalance();
+    const percentInput = document.getElementById('percent');
+    const percent = parseFloat(percentInput.value);
+    const totalSave = (income / 100) * percent;
+    const savingAmountField = document.getElementById('save-amount');
+    savingAmountField.innerText = totalSave;
+    const saveAmount = parseFloat(savingAmountField.innerText);
+    const remainingBalanceField = document.getElementById('remaining-balance');
+
+    if(balanceAmount < saveAmount) {
+        const saveErrorMessage = document.getElementById('save-error-message');
+        saveErrorMessage.innerText = "You don't have enough money to save";
+        savingAmountField.innerText = '0';
+        remainingBalanceField.innerText = '0';
+    }
+    else {
+        const remainingBalance = balanceAmount - saveAmount;
+        remainingBalanceField.innerText = remainingBalance;
+    }
+    
+}
 
 // Event listener on calculate button
 document.getElementById('calculate').addEventListener('click', function() {
     incomeExpense();
+})
+// Event listener on save button
+document.getElementById('save').addEventListener('click', function() {
+    calculateSave();
 })
